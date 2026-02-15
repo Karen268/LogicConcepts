4ddf1a2fd71c9e81bd4f44cb7ccd644f233dfcb4
KAREN TATIANA MARTINEZ CARDENAS <karenmartinez1157957@correo.itm.edu.co>
2026-02-15 15:29:09 -0500

    Complete exercise 01

---
+++ IsNumerOddOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿var numberString = String.Empty;
+do
+{
+    Console.Write("Ingrese número entero o la palabra 'salir' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt}, es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt}, es impar.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un número entero. ");
+    }
+} while (numberString!.ToLower() != "Salir");
+Console.WriteLine("Gracias.");
+
+
