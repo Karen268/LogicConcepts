b2f3ad95bbbe5211e203003ebc679877e8239a5f
KAREN TATIANA MARTINEZ CARDENAS <karenmartinez1157957@correo.itm.edu.co>
2026-02-16 17:31:07 -0500

    Exercise 08

---
+++ LogicConcepts/Subsidies/Program.cs
@@ -0,0 +1,67 @@
+﻿using Shared;
+
+var answer = String.Empty;
+var options = new List<string> { "s", { "n" } };
+
+do
+{
+    var credits = ConsoleExtension.GetInt($"Número de creditos..............: ");
+    var creditValue = ConsoleExtension.GetDecimal($"Valor credito...........: ");
+    var stratum = ConsoleExtension.GetInt($"Estrato del estudiante..........: ");
+
+    var registrationvalue = CalculateRegistrationValue(credits, creditValue, stratum);
+    var subsidy = CalculateSubsidy(stratum);
+
+    Console.WriteLine($"Costo de la matricula...............: {registrationvalue,20:C2}");
+    Console.WriteLine($"Valor del subsidio..................: {subsidy,20:C2}");
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Deseas continuar [S]i [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+decimal CalculateSubsidy(int stratum)
+{
+    if (stratum ==1)
+    {
+        return 200000m;
+    }
+    if (stratum ==2)
+    {
+        return 100000m;
+    }
+    return 0;
+}
+
+decimal CalculateRegistrationValue(int credits, decimal creditValue, int stratum)
+{
+    decimal value;
+    if (credits <= 20)
+    {
+        value = credits * creditValue;
+    }
+    else
+    {
+        value = 20 * creditValue + (credits - 20) * creditValue * 2;
+    }
+    if (stratum == 1)
+    {
+        return value * 0.2m;
+    }
+    if (stratum == 2)
+    {
+        return value * 0.5m;
+    }
+    if (stratum ==3)
+    {
+        return value * 0.7m;
+    }
+    return value;
+}
+
+Console.WriteLine("Gracias");
+
+
