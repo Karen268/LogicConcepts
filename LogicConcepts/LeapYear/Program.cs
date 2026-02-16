f94dd0aae36ea119fad4bf162c439440adeba9a3
KAREN TATIANA MARTINEZ CARDENAS <karenmartinez1157957@correo.itm.edu.co>
2026-02-16 13:23:56 -0500

    Exercise 05

---
+++ LogicConcepts/LeapYear/Program.cs
@@ -0,0 +1,44 @@
+﻿using Shared;
+
+do
+{
+    var CurrentYear = DateTime.Now.Year;
+    var Message = String.Empty;
+    var Year = ConsoleExtension.GetInt("Ingrese año: ");
+    
+    if (Year == CurrentYear)
+    {
+        Message = "es";
+    }
+    else if (Year > CurrentYear)
+    {
+        Message = "va a ser";
+    }
+    else 
+    {
+        Message = "fue";
+    }
+
+    if (Year % 4 == 0)
+    {
+        if (Year % 100 == 0)
+        {
+            if (Year % 400 == 0)
+            {
+                Console.WriteLine($"El año: {Year} Si {Message} bisiesto");
+            }
+            else
+            { 
+                Console.WriteLine($"El año: {Year} No {Message} bisiesto");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año: {Year} Si {Message} bisiesto");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El año: {Year} No {Message} bisiesto");
+    }
+} while (true);
