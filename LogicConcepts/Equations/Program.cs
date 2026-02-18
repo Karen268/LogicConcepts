9e766732d26dc1dc9b40f07437039cb7237eb3c7
KAREN TATIANA MARTINEZ CARDENAS <karenmartinez1157957@correo.itm.edu.co>
2026-02-17 21:57:12 -0500

    Exercise 16

---
+++ LogicConcepts/Equations/Program.cs
@@ -0,0 +1,36 @@
+﻿using Shared;
+using System.Runtime.Intrinsics.Arm;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    var a = ConsoleExtension.GetDouble("Ingrese valor de a: ");
+    var b = ConsoleExtension.GetDouble("Ingrese valor de b: ");
+    var c = ConsoleExtension.GetDouble("Ingrese valor de c: ");
+    var solution = QuadraticEquationSolution(a, b, c);
+    Console.WriteLine($"X1 = {solution.X1!:N5}");
+    Console.WriteLine($"X2 = {solution.X2!:N5}");
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?....: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+QuadraticEquationSolution QuadraticEquationSolution(double a, double b, double c)
+{
+    return new QuadraticEquationSolution
+    {
+        X1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a),
+        X2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a)
+    };
+
+}
+
+public class QuadraticEquationSolution
+{
+    public double X1 { get; set; }
+    public double X2 { get; set; }
+}
